c2fe35f209f9aec64cda68ef504ba6c49fd09516
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-06-23 21:16:43 +0100

    add base controller setup

---
+++ src/controllers/contacts.js
@@ -0,0 +1,13 @@
+import db from '../models';
+
+class contactsController {
+
+  /**
+   * @description - Method to fetch all contacts
+   * @param {object} req - request body
+   * @param {object} res - response body
+   */
+  static getAContact(req, res) {
+    const contactId = req.params.id
+  }
+}
