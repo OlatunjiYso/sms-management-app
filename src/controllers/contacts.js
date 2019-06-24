e979325fbef2473127d6639270ff4979fd94b180
Olatunji Yusuf <olatunjiyso@yahoo.com>
2019-06-24 11:58:41 +0100

    update contacts controller

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,13 +1,53 @@
 import db from '../models';
 
+const dbClient = db.client;
+
 class contactsController {
 
   /**
-   * @description - Method to fetch all contacts
+   * @description - Adds a new contact
    * @param {object} req - request body
    * @param {object} res - response body
    */
-  static getAContact(req, res) {
-    const contactId = req.params.id
+  static AddContact(req, res) {
+    const { phoneNumber, name } = req.body;
+    let sqlQuery = `INSERT INTO Contacts (Name, PhoneNo)
+    VALUES ('${name}', '${phoneNumber}');`;
+
+    dbClient.query(sqlQuery, (err, result) => {
+      if (err) {
+        res.status(501)
+          .json({
+              error: err.sqlMessage, 
+              msg: 'internal server error'
+          })
+      } else {
+        res.status(201).json({
+          msg: 'contact successfully added!',
+          affectedRows: result.affectedRows
+        })
+      }
+    });
+  }
+
+  static FetchAllContacts(req, res) {
+    let sqlQuery = `SELECT * FROM Contacts;`
+    dbClient.query(sqlQuery, (err, result) => {
+      if (err) {
+        res.status(501)
+          .json({
+              msg: 'internal server error',
+              error: err.sqlMessage
+          })
+      } else {
+          res.status(200)
+            .json({
+              msg: 'contacts found',
+              contacts: result
+            });
+      }
+    });
   }
 }
+
+export default contactsController;
